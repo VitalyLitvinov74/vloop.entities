a10c0b7cd7b5f56a3f3c7e02039a404c01185a12
VitalyLitvinov74 <cheltend201@yandex.ru>
2021-09-21 12:32:16 +0500

    created yii2 tag

---
+++ src/yii2/AbstractForm.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace vloop\entities\yii2;
+
+
+use vloop\entities\contracts\Form;
+use vloop\entities\exceptions\NotValidatedFields;
+use Yii;
+use yii\base\Model;
+
+class AbstractForm extends Model implements Form
+{
+    protected $method;
+
+    public function __construct($method = 'post', $config = [])
+    {
+        $this->method = $method;
+        parent::__construct($config);
+    }
+
+    /**
+     * @return array
+     * @throws NotValidatedFields
+     */
+    public function validatedFields(): array
+    {
+        $post = Yii::$app->request->post();
+        $get = Yii::$app->request->get();
+        if ($this->method == 'post') {
+            if ($this->load($post, '') and $this->validate()) {
+                return $this->getAttributes();
+            }
+        } elseif ($this->method == 'get') {
+            if ($this->load($get, '') and $this->validate()) {
+                return $this->getAttributes();
+            }
+        }
+        throw new NotValidatedFields($this->getErrors(), 400);
+    }
+}
