ef05f12d8d67bff898ac40a47a80e2e6f550cbb1
VitalyLitvinov74 <cheltend201@yandex.ru>
2021-10-20 10:00:25 +0500

    Этот коммит тоже временный, если что отменить

--- src/decorators/rest/jsonapi/Relation.php
+++ src/decorators/rest/jsonapi/Relation.php
@@ -6,9 +6,15 @@ namespace vloop\entities\decorators\rest\jsonapi;
 
 trait Relation
 {
-    private function moveToRelation(array &$dataValues, string $needleAttribute){
-        if(array_key_exists($needleAttribute, $dataValues['attributes'])){
-            $dataValues['relationships'][$needleAttribute]['data'] = $dataValues['attributes'][$needleAttribute];
+    use JsonDataTrait;
+
+    private function moveToRelation(array &$dataValues, string $needleAttribute)
+    {
+        if (array_key_exists($needleAttribute, $dataValues['attributes'])) {
+            $dataValues['relationships'][$needleAttribute]['data'] = $this->jsonDataFromArray(
+                $needleAttribute,
+                $dataValues['attributes'][$needleAttribute]
+            );
             unset($dataValues['attributes'][$needleAttribute]);
         }
     }
