37dfe177217a2fd18a519fd4566f099de6cf7fb0
VitalyLitvinov74 <cheltend201@yandex.ru>
2021-10-20 10:15:04 +0500

    Revert "Этот коммит тоже временный, если что отменить"
    This reverts commit ef05f12d8d67bff898ac40a47a80e2e6f550cbb1.

--- src/decorators/rest/jsonapi/Relation.php
+++ src/decorators/rest/jsonapi/Relation.php
@@ -6,15 +6,9 @@ namespace vloop\entities\decorators\rest\jsonapi;
 
 trait Relation
 {
-    use JsonDataTrait;
-
-    private function moveToRelation(array &$dataValues, string $needleAttribute)
-    {
-        if (array_key_exists($needleAttribute, $dataValues['attributes'])) {
-            $dataValues['relationships'][$needleAttribute]['data'] = $this->jsonDataFromArray(
-                $needleAttribute,
-                $dataValues['attributes'][$needleAttribute]
-            );
+    private function moveToRelation(array &$dataValues, string $needleAttribute){
+        if(array_key_exists($needleAttribute, $dataValues['attributes'])){
+            $dataValues['relationships'][$needleAttribute]['data'] = $dataValues['attributes'][$needleAttribute];
             unset($dataValues['attributes'][$needleAttribute]);
         }
     }
